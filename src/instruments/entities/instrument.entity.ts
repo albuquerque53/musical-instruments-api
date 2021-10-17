7ea3e8297ddc7da43af655f9302a7546b8d91d45
albuquerque53 <gabrielalbuquerque-dev@hotmail.com>
2021-10-17 12:59:21 -0300

    Configuring the instrument schemas into mongoose

--- src/instruments/entities/instrument.entity.ts
+++ src/instruments/entities/instrument.entity.ts
@@ -1,1 +1,24 @@
-export class Instrument {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type InstrumentDocument = Instrument & Document;
+
+@Schema()
+export class Instrument {
+  @Prop()
+  name: string;
+
+  @Prop()
+  type: string;
+
+  @Prop()
+  brand: string;
+
+  @Prop()
+  model: string;
+
+  @Prop()
+  color: string;
+}
+
+export const InstrumentSchema = SchemaFactory.createForClass(Instrument);
