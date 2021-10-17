4c493ce787a2d8c3903793de418eced23f2967ae
albuquerque53 <gabrielalbuquerque-dev@hotmail.com>
2021-10-17 12:58:17 -0300

    Configuring mongoose into application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { InstrumentsModule } from './instruments/instruments.module';
 
 @Module({
-  imports: [InstrumentsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    InstrumentsModule,
+    MongooseModule.forRoot(process.env.MONGOOSE_CONNECTION_URI),
+  ],
   controllers: [],
   providers: [],
 })
